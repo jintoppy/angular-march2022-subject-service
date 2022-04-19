35a4664e2b0f03b6e0bb79bbdeac501c783ff815
Jinto Jose <jintoppy@gmail.com>
2022-04-19 15:27:58 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule
